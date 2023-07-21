745f70772f0cd0e37e1312c843d9b5b36740d579
CrazyTnT <CrazyTnT21@gmail.com>
2023-07-21 18:06:59 +0200

    Environment variables

--- MyCollectionServer/Program.cs
+++ MyCollectionServer/Program.cs
@@ -1,28 +1,27 @@
-//#define Benchmark
-
 using System.Text.Json.Serialization;
 using Domain.Common;
 using Microsoft.Extensions.Logging.Console;
-using MyCollectionServer;
 using MyCollectionServer.Core;
 using MySqlConnector;
+
+string ip = Environment.GetEnvironmentVariable("IP") ?? throw new Exception($"Enviroment variable 'IP' is missing!");
+string user = Environment.GetEnvironmentVariable("USER") ??
+              throw new Exception($"Enviroment variable 'USER' is missing!");
+string password = Environment.GetEnvironmentVariable("PASSWORD") ??
+                  throw new Exception($"Enviroment variable 'PASSWORD' is missing!");
+string db = Environment.GetEnvironmentVariable("DB") ?? throw new Exception($"Enviroment variable 'DB' is missing!");
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddCors(options =>
 {
-  options.AddPolicy(name: "Server",
-    policy => { policy.WithOrigins("http://localhost:4200").AllowAnyHeader(); });
+  options.AddPolicy("Server", policy => policy.WithOrigins("http://localhost:4000").AllowAnyHeader());
 });
-
 using var loggerFactory = LoggerFactory.Create(loggerBuilder =>
 {
   loggerBuilder.AddSimpleConsole(i => i.ColorBehavior = LoggerColorBehavior.Enabled);
 });
-var con = new MySqlConnection($"server={args[0]};userid={args[1]};password={args[2]};database={args[3]}");
 var logger = loggerFactory.CreateLogger<Program>();
-await con.OpenAsync();
-builder.Services.AddSingleton<ILogger>(logger);
-builder.Services.AddSingleton(con);
+
 builder.Services.AddControllers().ConfigureApiBehaviorOptions(options =>
 {
   var builtInFactory = options.InvalidModelStateResponseFactory;
@@ -31,24 +30,23 @@ builder.Services.AddControllers().ConfigureApiBehaviorOptions(options =>
 
 builder.Services.AddControllers().AddJsonOptions(options =>
 {
-  //TODO: Remove once DateOnly support has been added
   options.JsonSerializerOptions.Converters.Add(new DateOnlyJsonConverter());
   options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
 });
 
+var connection = new MySqlConnection($"server={ip};userid={user};password={password};database={db}");
+
+builder.Services.AddSingleton<ILogger>(logger);
+builder.Services.AddSingleton(connection);
+
 var app = builder.Build();
 
-app.UseHttpsRedirection();
-app.UseCors("Server");
-app.UseAuthorization();
 app.MapControllers();
-// app.Urls.Add("");
+app.UseHttpsRedirection()
+  .UseCors("Server")
+  .UseAuthorization();
 app.MapGet("/api/ver", () => "myCollection.ver: 0.01");
 app.MapGet("/api/query", BaseT.getParams);
 app.MapGet("/api/lang", () => BaseT.languages);
-BaseT.languages =
-  (await QueryBase.QueryDB<Language>(new MySqlCommand("select Language, `Column` from Language", con))).ToArray();
-if (BaseT.languages.Length < 1)
-  logger.LogCritical("No Languages have been found, something probably went wrong! Languages: {lang}", BaseT.languages);
 
 app.Run();
